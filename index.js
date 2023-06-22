d6b69e7849bacc7b951b66942d3d1ebd9d3b5a6c
Your Name <jymohcooler@gmail.com>
2023-06-22 23:30:52 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const jane = document.getElementById("text")
+    jane.textContent ="This is really cool!"
+})
